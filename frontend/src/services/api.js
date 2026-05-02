c7d9c4d4e402efb6e92375fd3db6be526b8aa418
Vandana_Kumari <k.vandana2468@gmail.com>
2026-05-03 00:16:56 +0530

    frontend: added backend API url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for all API calls
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://taskflow-team-task-manager-ic08.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE,
